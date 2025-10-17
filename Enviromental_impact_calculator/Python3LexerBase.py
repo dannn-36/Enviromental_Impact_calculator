a5180b235f51a207adb2b9833507ab9016ca90d1
Daniel <josdanvidu@gmail.com>
2025-10-16 22:04:31 -0500

    commit ( Funcionan 2 de 5, java y C completamente funcionales, analisis estatico realizado correctamente, le falta afinamiento, los demás lenguajes no serven todavia)

--- Enviromental_impact_calculator/Python3LexerBase.py
+++ Enviromental_impact_calculator/Python3LexerBase.py
@@ -3,16 +3,3 @@ from antlr4 import Lexer
 class Python3LexerBase(Lexer):
     def __init__(self, input=None, output=None):
         super().__init__(input, output)
-        self.nesting = 0
-
-    def atStartOfInput(self):
-        return self._input.index == 0
-
-    def openBrace(self):
-        self.nesting += 1
-
-    def closeBrace(self):
-        self.nesting -= 1
-
-    def onNewLine(self):
-        pass
